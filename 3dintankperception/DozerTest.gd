7e5039949f29ac364171d86f4b1a594644e2e8d2
yukipookitamu <yuki@tamu.edu>
2024-10-28 10:24:53 -0500

    working rudimentary tank model

---
+++ 3dintankperception/DozerTest.gd
@@ -0,0 +1,23 @@
+extends VehicleBody3D
+
+var speed = 5
+var left
+var right
+
+func _ready():
+	pass
+	
+func _physics_process(delta: float) -> void:
+	
+	# get keyboard
+	left = int((Input.get_action_raw_strength("left_tread_forward")
+		- Input.get_action_raw_strength("left_tread_backward")) * 10)
+	right = int((Input.get_action_raw_strength("right_tread_forward")
+		- Input.get_action_raw_strength("right_tread_backward")) * 10)
+	
+	print(left)
+	print(right)
+	print("--------")
+	
+	$LeftWheel.engine_force = left * speed
+	$RightWheel.engine_force = right * speed
