56f7c90a27a3e94729f07629c26e0951aebda9c4
yukipookitamu <yuki@tamu.edu>
2024-10-28 13:03:27 -0500

    happy with this so far, collision shape having some odd behavior

--- 3dintankperception/tread.gd
+++ 3dintankperception/tread.gd
@@ -3,7 +3,7 @@ extends Path3D
 
 var time = 0
 var speed = 2
-var offset = 0.5
+var offset = 0.11
 
 @onready var links = $".".get_children()
 
@@ -13,6 +13,6 @@ func _process(delta):
 	
 	var count = 0
 	for index in links:
-		index.progress = speed + time + (offset * count)
-		
+		#index.progress = speed + time + (offset * count)
+		index.progress = offset * count
 		count += 1
