fb961995dd183c50295e628a198a2854d44fb6a9
yukiCodesStuff <yuki.janvier@gmail.com>
2024-11-01 16:28:16 -0500

    breaking blender model into multiple components

---
+++ 3dintankperception/m27_body_movement.gd
@@ -0,0 +1,23 @@
+extends MeshInstance3D
+
+var arm_speed = 0.005
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	#print(position.y)
+	
+	var elevation = 0
+	if Input.is_action_pressed("arm_down"):
+		if position.y >= 0.03635939955711:
+			elevation -= 1
+	if Input.is_action_pressed("arm_up"):
+		if position.y < 30:
+			elevation += 1
+	
+	position.y += elevation * arm_speed
