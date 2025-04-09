d304372fe80792747a56516097b2837cd9a285e0
Thomas Holt <tmholt02@gmail.com>
2025-04-08 19:53:32 -0500

    Gridlines! They appear to move, even though they dont. They appear to change scale, even though they dont!

--- Scripts/Coordinator.cs
+++ Scripts/Coordinator.cs
@@ -6,125 +6,125 @@ using System.Runtime.InteropServices;
 
 public partial class Coordinator : Node
 {
-    [Export] public CameraDragZoom WorldCamera;
-    [Export] public Node2D WorldRoot;
-    public bool IsReady { get; private set; } = false;
-    private Vector2 startPoint;
-    private Vector2 endPoint;
-    private float mass;
-    private float length;
-    private int segmentCount;
+	[Export] public GridlineController GridController;
+	[Export] public Node2D WorldRoot;
+	public bool IsReady { get; private set; } = false;
+	private Vector2 startPoint;
+	private Vector2 endPoint;
+	private float mass;
+	private float length;
+	private int segmentCount;
 
-    private List<CablePlotter> plotters = new List<CablePlotter>();
+	private List<CablePlotter> plotters = new List<CablePlotter>();
 
-    public void SetStartPointX(float value) => GD.Print(startPoint.X = value);
-    public void SetStartPointY(float value) => startPoint.Y = value;
-    public void SetEndPointX(float value) => endPoint.X = value;
-    public void SetEndPointY(float value) => endPoint.Y = value;
+	public void SetStartPointX(float value) => GD.Print(startPoint.X = value);
+	public void SetStartPointY(float value) => startPoint.Y = value;
+	public void SetEndPointX(float value) => endPoint.X = value;
+	public void SetEndPointY(float value) => endPoint.Y = value;
 
-    public void SetStartPoint(Vector2 value) => startPoint = value;
-    public void SetEndPoint(Vector2 value) => endPoint = value;
+	public void SetStartPoint(Vector2 value) => startPoint = value;
+	public void SetEndPoint(Vector2 value) => endPoint = value;
 
-    public void SetMass(float value) => mass = value;
-    public void SetLength(float value) => length = value;
-    public void SetSegmentCount(int value) => segmentCount = value;
+	public void SetMass(float value) => mass = value;
+	public void SetLength(float value) => length = value;
+	public void SetSegmentCount(int value) => segmentCount = value;
 
-    public float GetStartPointX() => startPoint.X;
-    public float GetStartPointY() => startPoint.Y;
-    public float GetEndPointX() => endPoint.X;
-    public float GetEndPointY() => endPoint.Y;
+	public float GetStartPointX() => startPoint.X;
+	public float GetStartPointY() => startPoint.Y;
+	public float GetEndPointX() => endPoint.X;
+	public float GetEndPointY() => endPoint.Y;
 
-    public Vector2 GetStartPoint() => startPoint;
-    public Vector2 GetEndPoint() => endPoint;
+	public Vector2 GetStartPoint() => startPoint;
+	public Vector2 GetEndPoint() => endPoint;
 
-    public float GetMass() => mass;
-    public float GetLength() => length;
-    public int GetSegmentCount() => segmentCount;
+	public float GetMass() => mass;
+	public float GetLength() => length;
+	public int GetSegmentCount() => segmentCount;
 
-    public void GeneratePlots()
-    {
-        GD.Print("GeneratePlots()...");
-        Vector2[] initalPoints = InitialCurve.Make(startPoint, endPoint, mass, length, segmentCount);
-        float nodeMass = mass / segmentCount;
-        foreach (CablePlotter plotter in plotters)
-        {
-            plotter.Generate(nodeMass, initalPoints);
-        }
-    }
+	public void GeneratePlots()
+	{
+		GD.Print("GeneratePlots()...");
+		Vector2[] initalPoints = InitialCurve.Make(startPoint, endPoint, mass, length, segmentCount);
+		float nodeMass = mass / segmentCount;
+		foreach (CablePlotter plotter in plotters)
+		{
+			plotter.Generate(nodeMass, initalPoints);
+		}
+	}
 
-    public void AddPlotter(CablePlotter plotter)
-    {
-        if (plotter is Node node)
-        {
-            WorldRoot.AddChild(node);
-        }
+	public void AddPlotter(CablePlotter plotter)
+	{
+		if (plotter is Node node)
+		{
+			WorldRoot.AddChild(node);
+		}
 
-        if (!plotters.Contains(plotter))
-        {
-            plotters.Add(plotter);
-        }
-    }
+		if (!plotters.Contains(plotter))
+		{
+			plotters.Add(plotter);
+		}
+	}
 
-    public void ClearPlotters()
-    {
-        plotters.Clear();
-    }
+	public void ClearPlotters()
+	{
+		plotters.Clear();
+	}
 
-    public void SetVisible(int index, bool visible)
-    {
-        if (index < 0 || index >= plotters.Count)
-        {
-            GD.PrintErr($"SetVisible: Index {index} out of range for plotters list.");
-            return;
-        }
+	public void SetVisible(int index, bool visible)
+	{
+		if (index < 0 || index >= plotters.Count)
+		{
+			GD.PrintErr($"SetVisible: Index {index} out of range for plotters list.");
+			return;
+		}
 
-        if (visible) {
-            GD.Print($"Showing {index}");
-            plotters[index].ShowPlot();
-        }
-        else {
-            GD.Print($"Hiding {index}");
-            plotters[index].HidePlot();
-        }
-    }
+		if (visible) {
+			GD.Print($"Showing {index}");
+			plotters[index].ShowPlot();
+		}
+		else {
+			GD.Print($"Hiding {index}");
+			plotters[index].HidePlot();
+		}
+	}
 
-    public void ResetCamera() => WorldCamera.ResetCamera();
+	public void ResetCamera() => GridController.ResetCamera();
 
-    public CablePlotter[] GetPlotters() {
-        if (!IsReady) {
-            throw new Exception("Cannot call GetPlotters before Coordinator is ready.");
-        }
-        return plotters.ToArray();
-    }
+	public CablePlotter[] GetPlotters() {
+		if (!IsReady) {
+			throw new Exception("Cannot call GetPlotters before Coordinator is ready.");
+		}
+		return plotters.ToArray();
+	}
 
-    public override void _Ready()
-    {
-        AddPlotter(new RawPlotter("Initial Plot", new Color(.7f, .5f, 0)));
-        AddPlotter(new FEMLine(new Color(0, 0, 1)));
-        IsReady = true;
-    }
+	public override void _Ready()
+	{
+		AddPlotter(new RawPlotter("Initial Plot", new Color(.7f, .5f, 0)));
+		AddPlotter(new FEMLine(new Color(0, 0, 1)));
+		IsReady = true;
+	}
 
-    public const float PixelsPerMeter = 75.0f;
+	public const float PixelsPerMeter = 75.0f;
 
-    public static Vector2 WorldToMeters(Vector2 worldPos)
-    {
-        return new Vector2(
-            worldPos.X / PixelsPerMeter,
-            -worldPos.Y / PixelsPerMeter
-        );
-    }
+	public static Vector2 WorldToMeters(Vector2 worldPos)
+	{
+		return new Vector2(
+			worldPos.X / PixelsPerMeter,
+			-worldPos.Y / PixelsPerMeter
+		);
+	}
 
-    public static float WorldToMetersX(float x) => x / PixelsPerMeter;
-    public static float WorldToMetersY(float y) => -y / PixelsPerMeter;
+	public static float WorldToMetersX(float x) => x / PixelsPerMeter;
+	public static float WorldToMetersY(float y) => -y / PixelsPerMeter;
 
-    public static Vector2 MetersToWorld(Vector2 meterPos)
-    {
-        return new Vector2(
-            meterPos.X * PixelsPerMeter,
-            -meterPos.Y * PixelsPerMeter
-        );
-    }
+	public static Vector2 MetersToWorld(Vector2 meterPos)
+	{
+		return new Vector2(
+			meterPos.X * PixelsPerMeter,
+			-meterPos.Y * PixelsPerMeter
+		);
+	}
 
-    public static float MetersToWorldX(float x) => x * PixelsPerMeter;
-    public static float MetersToWorldY(float y) => -y * PixelsPerMeter;
+	public static float MetersToWorldX(float x) => x * PixelsPerMeter;
+	public static float MetersToWorldY(float y) => -y * PixelsPerMeter;
 }
---
+++ Scripts/GridlineController.cs
@@ -0,0 +1,154 @@
+using Godot;
+using System;
+
+public partial class GridlineController : Node2D
+{
+	[Export] public NodePath WorldRootPath;
+	private Node2D worldRoot;
+
+	private bool isDragging = false;
+	private Vector2 previousMouseScreenPos;
+
+	private Vector2 homePosition;
+	private Vector2 homeScale;
+
+	[Export] public float ZoomStep = 0.1f;
+	[Export] public float MinZoom = 0.2f;
+	[Export] public float MaxZoom = 5f;
+	[Export] public float GridSpacing = 1.0f; // in meters
+	[Export] public Color GridColor = new Color(0.8f, 0.8f, 0.8f, 0.5f);
+	[Export] public Color TextColor = new Color(1f, 1f, 1f);
+
+	public override void _Ready()
+	{
+		worldRoot = GetNode<Node2D>(WorldRootPath);
+		homePosition = worldRoot.Position;
+		homeScale = worldRoot.Scale;
+
+		SetProcessInput(true);
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButton)
+		{
+			if (mouseButton.ButtonIndex == MouseButton.Left)
+			{
+				isDragging = mouseButton.Pressed;
+				previousMouseScreenPos = GetViewport().GetMousePosition();
+			}
+
+			if (mouseButton.ButtonIndex == MouseButton.WheelUp)
+				ApplyZoom(+ZoomStep);
+			else if (mouseButton.ButtonIndex == MouseButton.WheelDown)
+				ApplyZoom(-ZoomStep);
+		}
+
+		if (@event is InputEventMouseMotion && isDragging)
+		{
+			Vector2 currentMouseScreenPos = GetViewport().GetMousePosition();
+			Vector2 screenDelta = currentMouseScreenPos - previousMouseScreenPos;
+
+			worldRoot.Position += screenDelta;
+			previousMouseScreenPos = currentMouseScreenPos;
+		}
+
+		QueueRedraw();
+	}
+
+	private void ApplyZoom(float zoomChange)
+	{
+		float currentZoom = (worldRoot.Scale.X + worldRoot.Scale.Y) * 0.5f;
+		float scaledStep = zoomChange * currentZoom;
+
+		float newZoomX = worldRoot.Scale.X + scaledStep;
+		float newZoomY = worldRoot.Scale.Y + scaledStep;
+
+		newZoomX = Mathf.Clamp(newZoomX, MinZoom, MaxZoom);
+		newZoomY = Mathf.Clamp(newZoomY, MinZoom, MaxZoom);
+
+		worldRoot.Scale = new Vector2(newZoomX, newZoomY);
+	}
+
+	public void ResetCamera()
+	{
+		worldRoot.Position = homePosition;
+		worldRoot.Scale = homeScale;
+		QueueRedraw();
+	}
+
+	public override void _Draw()
+	{
+		Vector2 viewportSize = GetViewportRect().Size;
+		Vector2 scale = worldRoot.Scale;
+
+		// Position of worldRoot in screenspace
+		Vector2 centerWorld = -worldRoot.Position;
+
+		// Get screen bounds in world space
+		Vector2 topLeftScreen = - viewportSize / 2f;
+		Vector2 bottomRightScreen =  viewportSize / 2f;
+		Vector2 topLeftWorld = (centerWorld + topLeftScreen) / scale;
+		Vector2 bottomRightWorld = (centerWorld + bottomRightScreen) / scale;
+		Vector2 topLeftMeters = Coordinator.WorldToMeters(topLeftWorld);
+		Vector2 bottomRightMeters = Coordinator.WorldToMeters(bottomRightWorld);
+
+		// Round bounds outward for full meter coverage
+		int startX = Mathf.FloorToInt(topLeftMeters.X);
+		int endX = Mathf.CeilToInt(bottomRightMeters.X);
+		int startY = Mathf.FloorToInt(bottomRightMeters.Y);
+		int endY = Mathf.CeilToInt(topLeftMeters.Y);
+
+		// Draw vertical lines every 1m
+		for (int x = startX; x <= endX; x++)
+		{
+			Vector2 worldStart = Coordinator.MetersToWorld(new Vector2(x, startY));
+			Vector2 worldEnd   = Coordinator.MetersToWorld(new Vector2(x, endY));
+
+			Vector2 screenStart = worldStart * scale - centerWorld;
+			Vector2 screenEnd   = worldEnd   * scale - centerWorld;
+
+			if (x != 0)
+				DrawLine(screenStart, screenEnd, GridColor, 1.0f);
+			else
+				DrawLine(screenStart, screenEnd, Colors.Green, 1.0f); // Y axis
+		}
+
+		// Draw horizontal lines every 1m
+		for (int y = startY; y <= endY; y++)
+		{
+			Vector2 worldStart = Coordinator.MetersToWorld(new Vector2(startX, y));
+			Vector2 worldEnd   = Coordinator.MetersToWorld(new Vector2(endX, y));
+
+			Vector2 screenStart = worldStart * scale - centerWorld;
+			Vector2 screenEnd   = worldEnd   * scale - centerWorld;
+
+			if (y != 0)
+				DrawLine(screenStart, screenEnd, GridColor, 1.0f);
+			else
+				DrawLine(screenStart, screenEnd, Colors.Red, 1.0f); // X axis
+		}
+
+		// --- Cursor Position in Meters ---
+		Vector2 mouseViewportPos = GetViewport().GetMousePosition();
+		Vector2 mouseScreenPos = mouseViewportPos - viewportSize / 2f;
+		if (
+			mouseScreenPos.X < topLeftScreen.X || 
+			mouseScreenPos.X > bottomRightScreen.X || 
+			mouseScreenPos.Y < topLeftScreen.Y || 
+			mouseScreenPos.Y > bottomRightScreen.Y
+		) return;
+		Vector2 worldPos = (centerWorld + mouseScreenPos) / scale;
+		Vector2 metersPos = Coordinator.WorldToMeters(worldPos);
+
+		// Format nicely
+        string cursorText = $"Cursor: {metersPos.X:F2} m, {metersPos.Y:F2} m";
+
+		// Pick a font
+		var font = ThemeDB.FallbackFont;
+		Vector2 margin = new Vector2(10, -10);
+		Vector2 drawPos = new Vector2(margin.X + topLeftScreen.X, margin.Y + bottomRightScreen.Y);
+		DrawString(font, drawPos, cursorText, HorizontalAlignment.Left, -1, 16, TextColor);
+	}
+
+}
